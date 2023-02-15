565c14c8eb5f381814856ebddbf0ff4b66de4a22
Susanne Kunis <sukunis@uos.de>
2023-02-15 14:46:19 +0100

    update to python 3

--- scripts/ImportTagsFromJSON.py
+++ scripts/ImportTagsFromJSON.py
@@ -1,19 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-This script can be used with OMERO version 5.2.x or newer.
-
-This script import tags by an JSON-file created by ExportTagsToJSON.py
-
-Attention: Please adapt parseDirName to your system
-
-@author Susanne Kunis
-<a href="mailto:susebo@gmail.com">susebo@gmail.com</a>
-@version 1.0
-"""
 import omero
-import os 
+import os
 from omero.gateway import BlitzGateway
-from omero.rtypes import rstring,rlong
+from omero.rtypes import rstring,unwrap,rlong,robject
 import omero.scripts as scripts
 import subprocess
 import sys
@@ -22,6 +10,33 @@ from omero_model_TagAnnotationI import TagAnnotationI
 from omero_model_AnnotationAnnotationLinkI import AnnotationAnnotationLinkI
 
 # omero.plugins.tag
+omeroFiles_Path="/OMERO_dev/Files/"
+SQL_Tag="""
+        select a.id, a.description, a.textValue,
+        a.details.owner.id, a.details.owner.firstName,
+        a.details.owner.lastName
+        from TagAnnotation a where a.id not in
+        (select distinct l.child.id from AnnotationAnnotationLink l)
+        and a.id not in
+        (select distinct l.parent.id from AnnotationAnnotationLink l)
+        and a.details.owner.id=:eid
+        """
+        
+SQL_TagSet="""
+    select distinct a.id, a.description, a.textValue
+    from TagAnnotation a, AnnotationAnnotationLink b
+    where a.id = b.parent.id
+    and a.ns=:ns
+    and a.details.owner.id=:eid
+    """
+    
+SQL_TagSetChilds="""
+    select a.id,a.description,a.textValue
+    from TagAnnotation a
+    where a.id in
+    (select distinct b.child.id from AnnotationAnnotationLink b
+    where b.parent.id=:pid)
+    """
 
 def getTagFile(conn,object_id,file_id):
     omero_object = conn.getObject("Project", int(object_id))
@@ -31,10 +46,10 @@ def getTagFile(conn,object_id,file_id):
     file = None
     for ann in omero_object.listAnnotations():
         if isinstance(ann, omero.gateway.FileAnnotationWrapper):
-            print "Load File ID:", ann.getFile().getId(), ann.getFile().getName(),\
-                "Size:", ann.getFile().getSize()
+            print("Load File ID: %d, %s Size: %d"%( ann.getFile().getId(), ann.getFile().getName(),
+                                                     ann.getFile().getSize()))
             if (ann.getFile().getId() == int(file_id)):
-                print "Identify tag file"
+                print ("Identify tag file")
                 file = ann.getFile()._obj
     if file is None:
         sys.stderr.write("Error: File does not exist.\n")
@@ -42,6 +57,86 @@ def getTagFile(conn,object_id,file_id):
     return file
 
 
+#returns [false,None] if tag doens't exist yet, [true,tag ID] otherwise
+def tagAvailable(client,conn,tagName):
+    #print("check if tag %s is still available"%(tagName))
+    
+    params = omero.sys.ParametersI()
+    params.addString('ns', omero.constants.metadata.NSINSIGHTTAGSET)
+    params.map['eid']=rlong(long(conn.getUser().getId()))
+    ice_map = dict()
+
+    session = client.getSession()
+    q = session.getQueryService()
+
+    for element in q.projection(SQL_Tag, params, ice_map):
+        tag_id, description, text, owner, first, last = map(unwrap, element)
+        if tagName == text:
+            return True,tag_id;
+    #end for
+    
+    # search also in tagsets childs
+    for element in q.projection(SQL_TagSet,params,ice_map):
+        tagset_id, description, text = map(unwrap, element)
+        params.map['pid']=rlong(long(tagset_id))
+        for child in q.projection(SQL_TagSetChilds,params,ice_map):
+            tag_id,tag_desc,tag_text=map(unwrap,child)
+            if tagName == tag_text:
+                return True,tag_id;
+            #end if
+        #end for
+    #end for
+    return False,None;
+
+#returns [false,None] if tag doens't exist yet, [true,tag ID] otherwise
+def tagSetAvailable(client,conn,tagSetName):
+    #print("check if tagSet %s is still available"%(tagSetName))
+    
+    params = omero.sys.ParametersI()
+    params.addString('ns', omero.constants.metadata.NSINSIGHTTAGSET)
+    params.map['eid']=rlong(long(conn.getUser().getId()))
+    ice_map = dict()
+
+    session = client.getSession()
+    q = session.getQueryService()
+
+    for element in q.projection(SQL_TagSet, params, ice_map):
+        tagSet_id, description, text = map(unwrap, element)
+        if tagSetName == text:
+            return True,tagSet_id;
+    #end for
+    return False,None;
+
+def listTagSets(client,conn):
+    print ("list available tags:")
+    params = omero.sys.ParametersI()
+    params.addString('ns', omero.constants.metadata.NSINSIGHTTAGSET)
+    params.map['eid']=rlong(long(conn.getUser().getId()))
+    ice_map = dict()
+
+    session = client.getSession()
+    q = session.getQueryService()
+
+    # gives all tags are not in tagesets:
+    for element in q.projection(SQL_Tag, params, ice_map):
+        tag_id, description, text, owner, first, last = map(unwrap, element)
+        print ("\tTag:",text)
+    #end for
+
+    
+    for element in q.projection(SQL_TagSet,params,ice_map):
+        tagset_id, description, text = map(unwrap, element)
+        
+        print ('\tTagset:',text)
+        params.map['pid']=rlong(long(tagset_id))
+      
+        for child in q.projection(SQL_TagSetChilds,params,ice_map):
+            tag_id,tag_desc,tag_text=map(unwrap,child)
+            print ('\t\tChild',tag_text)
+        # end for
+     
+    #end for
+ 
 
 def load(conn,filepath):
     """
@@ -52,82 +147,120 @@ def load(conn,filepath):
     else:
         sys.stderr.write("Error: No file is given.\n")
         sys.exit(1)
+    #end ifelse
 
     p = json.load(fobj)
 
 
     if fobj is not sys.stdin:
         fobj.close()
+    #end if
 
+   
 
     update = conn.getUpdateService()
     tagList2=[]
     for tset in p:
+        # {tag:<tagName>,desc:<description>}
         if "tag" in tset:
             tag=TagAnnotationI()
             tag.setTextValue(rstring(tset["tag"]))
             if tset["desc"]:
                 tag.setDescription(rstring(tset["desc"]))
-            tagList2.append(tag)
+            #end if
+            available,id = tagAvailable(client,conn,tset["tag"])
+            if not available:
+                print ("CREATE tag ",tset['tag'])
+                tagList2.append(tag)
+            else:
+                print("DON'T CREATE: %s - Tag still available [%id] " % (tset["tag"],id))
+            #end ifelse
         #end if
 
         if "tagset" in tset:
-            tagList=[]
+            childTagList=[]
+            # get child tags
+            #{tags:[{name:<tagName>,desc:<description>}],tagset:<tagSetName>,desc:<description>}
             for t in tset['tags']:
-                tag=TagAnnotationI()
-
-                tag.setTextValue(rstring(t["name"]))
-                if t["desc"]:
-                    tag.setDescription(rstring(t["desc"]))
-                tagList.append(tag)
+                try:
+                    tag=TagAnnotationI()
+    
+                    tag.setTextValue(rstring(t["name"]))
+                    if t["desc"]:
+                        tag.setDescription(rstring(t["desc"]))
+                    #end if
+                    available,id = tagAvailable(client,conn,t["name"])
+                    if not available:
+                        print ("CREATE childtag ",t['name'])
+                        childTagList.append(tag)
+                    else:
+                        print("DON'T CREATE: %s - ChildTag still available [%id] " % (t["name"],id))
+                    #end ifelse
+                except:
+                    print ("ERROR: Can't parse ",t)
+                #end try
+                
             #end for
-            tagList=update.saveAndReturnArray(tagList)
+            childTagList=update.saveAndReturnArray(childTagList)
 
-            tag=TagAnnotationI()
-            tag.setTextValue(rstring(tset["tagset"]))
-            if tset["desc"]:
-                tag.setDescription(rstring(tset["desc"]))
-            tag.setNs(rstring(omero.constants.metadata.NSINSIGHTTAGSET))
-            tag = update.saveAndReturnObject(tag)
-            links=[]
-            for child in tagList:
-                l=AnnotationAnnotationLinkI()
-                l.setChild(child)
-                l.setParent(tag)
-                links.append(l)
-            #end for
-            update.saveAndReturnArray(links)
+            # create tagset
+            available,id = tagSetAvailable(client,conn,tset["tagset"])
+            if not available:
+                print ("CREATE tagSet ",tset['tagset'])
+                tagSet=TagAnnotationI()
+                tagSet.setTextValue(rstring(tset["tagset"]))
+                if tset["desc"]:
+                    tagSet.setDescription(rstring(tset["desc"]))
+                #end if
+                tagSet.setNs(rstring(omero.constants.metadata.NSINSIGHTTAGSET))
+                tagSet = update.saveAndReturnObject(tagSet)
+                links=[]
+                for child in childTagList:
+                    l=AnnotationAnnotationLinkI()
+                    l.setChild(child)
+                    l.setParent(tagSet)
+                    links.append(l)
+                #end for
+                update.saveAndReturnArray(links)
+            else:
+                print("DON'T CREATE: %s - TagSet still available [%s] " % (tset["tagset"],id))
+                tagSet=TagAnnotationI(id,False)
+                links=[]
+                for child in childTagList:
+                    l=AnnotationAnnotationLinkI()
+                    l.setChild(child)
+                    l.setParent(tagSet)
+                    links.append(l)
+                #end for
+                update.saveAndReturnArray(links)
+                    
+            #end ifelse
         #end if
     #end for
     update.saveAndReturnArray(tagList2)
-
-def parseDirName(fileIdString):
-    fileIdNum=int(fileIdString)
-    if fileIdNum<1000:
-        return "/data/omero/Files/Dir-000/"+fileIdString
-    elif fileIdNum < 10000:
-        return "/data/omero/Files/Dir-00"+fileIdString[:1]+"/"+fileIdString
-    elif fileIdNum<100000:
-        return "/data/omero/Files/Dir-0"+fileIdString[:2]+"/"+fileIdString
-    else:
-        return "/data/omero/Files/Dir-"+fielIdString[:3]+"/"+fileIdString
-
 
 
 def importTags(client,conn,script_params):
     obj_id=0 #long(script_params["ID"])
     file_id=0
     fileID=scriptParams["FileId"]
-    myPath=parseDirName(fileID)
+    #myPath=parseDirName(fileID)
+    #p=subprocess.check_output("find /OMERO/Files/ -iname '"+fileID+"'", shell=True)
+    #print "Output find ","["+p+"]"
+#    myPath = [line[2:] for line in subprocess.check_output("find /OMERO/Files/ -iname '"+fileID+"'", shell=True).splitlines()]
+    myPath = [line[0:] for line in subprocess.check_output("find /storage/OMERO/Files/ -iname '"+fileID+"'", shell=True).splitlines()]
+    print ("Path: ",myPath)
+    
+    # define path to local json file
 #    file_path=scriptParams["Filepath"]
-    if not os.path.exists(myPath):
-        sys.stderr.write("Path doesn't exists: "+myPath)
-        sys.exit(1)
+    if myPath is None or len(myPath) == 0:
+        sys.stderr.write("File doesn't exists! Maybe path to /storage/OMERO/Files is wrong?")
     else:
-        load(conn,myPath)
-
-
-
+        if  os.path.isfile(myPath[0]):
+            load(conn,myPath[0])
+        else:
+            sys.stderr.write("File doesn't exists: "+myPath[0])
+            sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -139,7 +272,8 @@ if __name__ == "__main__":
         See also ExportTagsToJSON.
         """,
     scripts.String(
-        "FileId",optional=False,grouping='01',description="See tooltip of annotation file to get the File ID"),
+        "FileId",optional=False,grouping='01',description="See tooltip of annotation file to get the File ID"
+    ),
 
         version="1.0",
         authors=["Susanne Kunis"],
@@ -154,10 +288,13 @@ if __name__ == "__main__":
         for key in client.getInputKeys():
             if client.getInput(key):
                 scriptParams[key] = client.getInput(key, unwrap=True)
-        print scriptParams
+        print (scriptParams)
 
         # wrap client to use the Blitz Gateway
         conn = BlitzGateway(client_obj=client)
+        #list available tags
+        listTagSets(client,conn)
+        
         message = importTags(client, conn, scriptParams)
         client.setOutput("Message", rstring(message))
 
